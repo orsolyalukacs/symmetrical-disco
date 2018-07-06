838844ba8d12370f5bf6a5056ec5f693161ca0cf
orchie lucas <orchie.lucas@gmail.com>
2018-07-06 16:16:54 +0200

    setup of the express js server

---
+++ server/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "extends": "standard",
+    "parserOptions": {
+        "ecmaVersion": 6
+    },
+    "rules": {
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "allow": [
+            "log, warn, error"
+        ]
+    }
+};
