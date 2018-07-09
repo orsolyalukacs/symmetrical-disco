d87175b297dab32cb7c277e513b7551106a37a5e
orchie lucas <orchie.lucas@gmail.com>
2018-07-09 16:02:06 +0200

    front-end for user registration with policies and errors

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -4,7 +4,9 @@
       <input type="email" name="email" v-model="email" placeholder = "email"/>
       <input type="password" name="password" v-model="password" placeholder = "password"/>
       <br>
-  <button @click="register">Register</button>
+      <div class="error" v-html="error" />
+      <br>
+      <button @click="register">Register</button>
   </div>
 </template>
 <script>
@@ -12,18 +14,27 @@ import AuthenticationService from '@/services/AuthenticationService'
 export default {
   data () {
     return {
-      email: '', password: ''
+      email: '',
+      password: '',
+      error: null
     }
   },
   methods: {
     async register () {
-      await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
 </script>
 <style scoped>
+  .error{
+    color: red;
+  }
 </style>
