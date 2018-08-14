cad9e79e671792a4899a4cb80e135e2370548cc0
orchie lucas <orchie.lucas@gmail.com>
2018-08-14 10:05:05 +0200

    added SongsPanel instead of Songs

---
+++ client/src/components/Songs/Index.vue
@@ -0,0 +1,50 @@
+
+<template>
+    <v-layout column>
+        <v-flex xs6 offset-xs3>
+          <songs-search-panel />
+          <songs-panel />
+        </v-flex>
+  </v-layout>
+</template>
+<script>
+import SongsPanel from './SongsPanel'
+import SongsService from '@/services/SongsService'
+import SongsSearchPanel from './SongsSearchPanel'
+export default {
+  components: {
+    SongsPanel,
+    SongsSearchPanel
+  },
+  data () {
+    return {
+      songs: null
+    }
+  },
+  async mounted () {
+    // do a request to the backend for all the songs
+    this.songs = (await SongsService.index()).data
+    console.log('songs', this.songs)
+  }
+}
+</script>
+<style scoped>
+  .song{
+    padding: 20px;
+    height: 330px;
+    overflow: hidden;
+  }
+  .song-title{
+  font-size: 30px;
+  }
+  .song-artist{
+  font-size: 24px;
+  }
+  .song-genre{
+  font-size: 18px;
+  }
+  .album-image{
+      width: 70%;
+      margin: 0 auto;
+  }
+</style>
