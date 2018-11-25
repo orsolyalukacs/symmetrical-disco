bfbc7e55cb7988eb8519949c7afbb89a394519df
Orsolya Lucas <orchie.lucas@gmail.com>
2018-11-25 18:19:43 +0100

    Updated linter from windows to unix.

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
     "rules": {
         "linebreak-style": [
             "error",
-            "windows"
+            "unix"
         ],
         "quotes": [
             "error",
